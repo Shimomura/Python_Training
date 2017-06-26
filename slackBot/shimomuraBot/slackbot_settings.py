d750cb70d55115c13b7943b787f9d4a9e464ba99
a.shimomura <akitaka.shimomura@gmail.com>
2017-06-26 23:37:14 +0900

    slackbot HelloWorld

---
+++ slackBot/shimomuraBot/slackbot_settings.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import sys
+sys.path.insert(0, ".")
+
+
+# botアカウントのトークンを指定
+API_TOKEN = "xoxb-202983314017-HuIV7YxLIEjPDA776Vta6B67"
+
+# このbot宛のメッセージで、どの応答にも当てはまらない場合の応答文字列
+DEFAULT_REPLY = "お前が思うんならそうなんだろう お前ん中ではな"
+
+# プラグインスクリプトを置いてあるサブディレクトリ名のリスト
+PLUGINS = ['plugins']
